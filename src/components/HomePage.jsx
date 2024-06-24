5aa663cff307511a6b63183a1f357a2d6330a30b
brainka <brainkarau@gmail.com>
2024-06-24 21:34:19 +1000

    updating styling to match figma - wip

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,32 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import htmlLogo from '../assets/images/icon-html.svg';
+import cssLogo from '../assets/images/icon-css.svg';
+import jsLogo from '../assets/images/icon-js.svg';
+import accessibilityLogo from '../assets/images/icon-accessibility.svg';
 
 const HomePage = () => {
+	const [topics, setTopics] = useState([]);
+	const topicImages = {
+		html: htmlLogo,
+		css: cssLogo,
+		javascript: jsLogo,
+		accessibility: accessibilityLogo,
+	};
+
+	useEffect(() => {
+		async function fetchData() {
+			const url = '/data.json';
+			const response = await fetch(url);
+			const data = await response.json();
+			console.log(data.quizzes);
+			setTopics((topics) => [...data.quizzes.map((topic) => topic.title)]);
+		}
+		fetchData();
+	}, []);
+
+	topics.map((ele) => console.log(ele));
+
 	return (
 		<div className="homepage">
 			<div className="homepage-left">
@@ -12,19 +37,24 @@ const HomePage = () => {
 					<span>Pick a subject to get started.</span>
 				</p>
 			</div>
+
 			<div className="homepage-right">
-				<Link to="/quiz/html" state={{ topic: 'HTML' }}>
-					HTML
-				</Link>
-				<Link to="/quiz/css" state={{ topic: 'CSS' }}>
-					CSS
-				</Link>
-				<Link to="/quiz/javascript" state={{ topic: 'JavaScript' }}>
-					Javascript
-				</Link>
-				<Link to="/quiz/accessibility" state={{ topic: 'Accessibility' }}>
-					Accessibility
-				</Link>
+				{topics.map((topic) => {
+					const topicLowercase = topic.toLowerCase();
+					const topicImage = topicImages[topicLowercase];
+					return (
+						<Link
+							key={topic}
+							to={`/quiz/${topicLowercase}`}
+							state={{ topic: topic }}
+						>
+							<div className='image-container'>
+								<img className={topic} src={topicImage} alt="" />
+							</div>
+							{topic}
+						</Link>
+					);
+				})}
 			</div>
 		</div>
 	);
--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -4,18 +4,21 @@ import { useLocation } from 'react-router';
 const QuizPage = () => {
 	const location = useLocation();
 	const { topic } = location.state;
+
+	console.log(topic);
+
 	const [quizData, setQuizData] = useState([]);
 	const [quizQuestions, setquizQuestions] = useState([]);
 	const [currentIndex, setCurrentIndex] = useState(0);
 	const questionOrder = ['A', 'B', 'C', 'D'];
-	const [selectedAnswer, setSelectedAnswer] = useState(undefined);
-	const [selectedAnswers, setSelectedAnswers] = useState({
+	const [selectedAnswer, setselectedAnswer] = useState({
 		answerSelected: undefined,
 		buttonSelected: undefined,
 	});
 	const [quizAnswer, setQuizAnswer] = useState('');
 	const [submittedAnswer, setSubmittedAnswer] = useState(false);
 	const [correct, setCorrect] = useState();
+	const [score, setScore] = useState(0);
 
 	useEffect(() => {
 		async function fetchData() {
@@ -37,81 +40,134 @@ const QuizPage = () => {
 		fetchData();
 	}, [topic, currentIndex]);
 
-	// console.log(quizAnswer);
-
 	function incrementCurrentIndex() {
 		setCurrentIndex(currentIndex + 1);
 		setSubmittedAnswer(false);
+		setselectedAnswer({});
 	}
 
 	function handleAnswerSelection(e, buttonIndexSelected) {
 		console.log(buttonIndexSelected);
 		console.log(e.target.innerText);
 		// setSelectedAnswer(e.target.innerText);
-		setSelectedAnswers({
-			answerSelected: e.target.innerText,
-			buttonSelected: buttonIndexSelected,
-		});
+		!submittedAnswer &&
+			setselectedAnswer({
+				answerSelected: e.currentTarget.querySelector('button').innerText,
+				buttonSelected: buttonIndexSelected,
+			});
 	}
 
-	console.log(selectedAnswers.answerSelected);
+	console.log(selectedAnswer.answerSelected);
 
 	function handleAnswerSubmit() {
 		// console.log('submitted answer');
-		console.log(selectedAnswers.answerSelected);
+
+		console.log(selectedAnswer.answerSelected);
 		setSubmittedAnswer(true);
 
 		//you can only submit once
-		if (selectedAnswers.answerSelected === quizAnswer) {
+		if (selectedAnswer.answerSelected === quizAnswer) {
 			console.log('correct answer');
 			setCorrect(true);
+			setScore(score + 1);
 
 			//set style to green
-		} else if (selectedAnswers.answerSelected !== quizAnswer) {
+		} else if (selectedAnswer.answerSelected !== quizAnswer) {
 			//set it to red
 			console.log('wrong answer');
 			setCorrect(false);
+			//need to display the correct answer
 		}
 	}
 
+	function getButtonClass(
+		submittedAnswer,
+		quizQuestion,
+		quizAnswer,
+		index,
+		selectedAnswer,
+		correct
+	) {
+		if (index === selectedAnswer.buttonSelected) {
+			console.log('second hello');
+			if (submittedAnswer) {
+				return correct ? 'correct' : 'incorrect';
+			}
+			console.log('third hello');
+			return 'active-selected';
+		}
+
+		if (submittedAnswer && quizQuestion === quizAnswer) {
+			console.log('first hello');
+			return 'not-selected-correct-answer ';
+		}
+		return '';
+	}
+
+	function QuizOption({ questionOrder, index, submittedAnswer, quizQuestion }) {
+		return (
+			<div
+				// className="quiz-button-container"
+				key={index}
+				onClick={(e) => handleAnswerSelection(e, index)}
+				disabled={submittedAnswer}
+				className={`quiz-button-container ${getButtonClass(
+					submittedAnswer,
+					quizQuestion,
+					quizAnswer,
+					index,
+					selectedAnswer,
+					correct
+				)}`}
+			>
+				<div className="question-order">{questionOrder[index]}</div>
+				<button>{quizQuestion}</button>
+			</div>
+		);
+	}
+
 	return (
-		<div>
-			{quizAnswer}
-			{`Question ${currentIndex + 1} of ${quizQuestions.length}`}
-			{quizQuestions[currentIndex]?.question}
-			<input
-				id="completed-percentage"
-				type="range"
-				min={0}
-				max={quizQuestions.length}
-				value={currentIndex}
-				readOnly
-			></input>
-			{quizQuestions[currentIndex]?.options.map((element, index) => (
-				<div key={index}>
-					<div>{questionOrder[index]}</div>
-					<button
-						onClick={(e) => handleAnswerSelection(e, index)}
-						className={
-							index === selectedAnswers.buttonSelected
-								? submittedAnswer
-									? correct
-										? 'correct'
-										: 'incorrect'
-									: ''
-								: ''
-						}
-						disabled={submittedAnswer}
-					>
-						{element}
-					</button>
+		<div className="quiz-page-container">
+			<div className="quiz-page-container-left">
+				<div className="quiz-info-container">
+					<div className="question-counter">{`Question ${currentIndex + 1} of ${
+						quizQuestions.length
+					}`}</div>
+					{/* {score} */}
+					<div className="quiz-question">
+						{quizQuestions[currentIndex]?.question}
+					</div>
 				</div>
-			))}
-			{!submittedAnswer ? (
-				<button onClick={handleAnswerSubmit}>Submit Answer</button>
-			) : (
-				<button onClick={incrementCurrentIndex}>Next Question</button>
-			)}
+				<input
+					id="completed-percentage"
+					type="range"
+					min={0}
+					max={quizQuestions.length}
+					value={currentIndex}
+					// readOnly
+				/>
+			</div>
+			<div className="quiz-page-container-right">
+				{' '}
+				{quizQuestions[currentIndex]?.options.map((quizQuestion, index) => (
+					<QuizOption
+						key={index}
+						questionOrder={questionOrder}
+						index={index}
+						submittedAnswer={submittedAnswer}
+						quizQuestion={quizQuestion}
+					/>
+				))}
+				{!submittedAnswer ? (
+					<button className="button-submit" onClick={handleAnswerSubmit}>
+						Submit Answer
+					</button>
+				) : (
+					<button className="button-submit" onClick={incrementCurrentIndex}>
+						Next Question
+					</button>
+				)}
+			</div>
 		</div>
 	);
 };
